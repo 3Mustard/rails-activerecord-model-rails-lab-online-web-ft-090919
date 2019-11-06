3ab63cc27851ab017e248588598e762541cf702e
Matthew C <m23.cullen@gmail.com>
2019-11-06 16:14:17 +0000

    Automatically backed up by Learn

--- db/migrate/20191106161146_create_students_table.rb
+++ db/migrate/20191106161146_create_students_table.rb
@@ -1,7 +1,6 @@
 class CreateStudentsTable < ActiveRecord::Migration[5.0]
   def change
-    create_table :students_tables do |t|
-      t.string :students
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
     end
